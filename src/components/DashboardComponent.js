5749c8a49065e962ff73586f21114fcd8e9539c3
ZanyuanYang <59616360+ZanyuanYang@users.noreply.github.com>
2022-11-03 13:18:02 -0700

    CS5500G4-19 add credential for the page need to login

--- src/components/DashboardComponent.js
+++ src/components/DashboardComponent.js
@@ -1,34 +1,35 @@
 import React from 'react'
-import Wrapper from '../asserts/wrappers/DashboardComponent';
-import Button from '@mui/material/Button';
-import avatar from '../asserts/images/user.png';
-
+import Wrapper from '../asserts/wrappers/DashboardComponent'
+import Button from '@mui/material/Button'
+import avatar from '../asserts/images/user.png'
+import { RMCContext } from '../context/context'
 
 const DashboardComponent = () => {
-    return (
-        <Wrapper>
-            <div className="user">
-                <h1>Hey, Jaying!</h1>
-                <img src={avatar} alt="avatar" />
-            </div>
-            <div className="action">
-                <h3>I want to...</h3>
-                <Button variant="contained">Add a Course</Button>
-                <Button variant="contained">View Courses</Button>
-                <Button variant="contained">Edit Profile</Button>
-            </div>
+  const { user } = React.useContext(RMCContext)
+  return (
+    <Wrapper>
+      <div className="user">
+        <h1>Hey, {user.username}!</h1>
+        <img src={avatar} alt="avatar" />
+      </div>
+      <div className="action">
+        <h3>I want to...</h3>
+        <Button variant="contained">Add a Course</Button>
+        <Button variant="contained">View Courses</Button>
+        <Button variant="contained">Edit Profile</Button>
+      </div>
 
-            <div className="course">
-                <h3>My Courses</h3>
-                <div className="course_list">
-    {/* todo: Update the courses that link to each course's detail page */}
-                    <a href="">Course 1 - Course Name</a>
-                    <a href="">Course 2 - Course Name</a>
-                    <a href="">Course 3 - Course Name</a>
-                </div>
-            </div>
-        </Wrapper>
-    );
+      <div className="course">
+        <h3>My Courses</h3>
+        <div className="course_list">
+          {/* todo: Update the courses that link to each course's detail page */}
+          <a href="">Course 1 - Course Name</a>
+          <a href="">Course 2 - Course Name</a>
+          <a href="">Course 3 - Course Name</a>
+        </div>
+      </div>
+    </Wrapper>
+  )
 }
 
 export default DashboardComponent
--- src/components/SignupComponent.js
+++ src/components/SignupComponent.js
@@ -1,49 +1,41 @@
-import React, {useState, useEffect} from 'react'
-import VisibilityIcon from '@mui/icons-material/Visibility';
-import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
-import {Link} from "react-router-dom";
-import Wrapper from "../asserts/wrappers/SignUpAndLogIn";
+import React, { useState, useEffect } from 'react'
+import VisibilityIcon from '@mui/icons-material/Visibility'
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff'
+import { Link } from 'react-router-dom'
+import Wrapper from '../asserts/wrappers/SignUpAndLogIn'
 import register from '../asserts/images/register.png'
-// import { BASE_URL } from '../utils/constant'
-import axios from 'axios';
-// import { JobLandingContext } from '../context/context';
-// import useRequest from '../hooks/use-request';
-
+import { BASE_URL } from '../utils/constant'
+import axios from 'axios'
+import { RMCContext } from '../context/context';
+import useRequest from '../utils/use-request'
 
 const initialState = {
   name: '',
   email: '',
-  password: ''
+  password: '',
 }
-// const validateState = {
-//   character: false,
-//   number: false,
-//   uppercase: false,
-//   lowercase: false
-// }
 const SignupComponent = () => {
-//   const { login } = React.useContext(JobLandingContext);
-  const [values, setValues] = useState(initialState);
-  const [showPassword, setShowPassword] = useState(false);
-  const [loading, setLoading] = useState(false);
-  // const [passwordValidate, setPasswordValidate] = useState(validateState);
+  const { login } = React.useContext(RMCContext)
+  const [values, setValues] = useState(initialState)
+  const [showPassword, setShowPassword] = useState(false)
+  const [loading, setLoading] = useState(false)
 
-//   const { doRequest, errors } = useRequest({
-//     url: `${BASE_URL}/user/signup`,
-//     method: 'post',
-//     body: {
-//       username: values.name,
-//       email: values.email,
-//       password: values.password
-//     },
-//     onSuccess: (data) => {
-//       setLoading(true)
-//         setTimeout(() => {
-//           setLoading(false);
-//           loginValidator(values.email, values.password)
-//         }, 2000);
-//     },
-//   });
+  const { doRequest, errors } = useRequest({
+    url: `${BASE_URL}/user/signup`,
+    method: 'post',
+    body: {
+      username: values.name,
+      email: values.email,
+      password: values.password,
+    },
+    onSuccess: (data) => {
+      setLoading(true)
+      setTimeout(() => {
+        setLoading(false)
+        loginValidator(values.email, values.password)
+      }, 2000)
+    },
+  })
 
   const handleChange = (e) => {
     setValues({ ...values, [e.target.name]: e.target.value })
@@ -51,53 +43,90 @@ const SignupComponent = () => {
 
   const handleClickShowPassword = () => {
     setShowPassword(!showPassword)
-  };
+  }
 
-//   const loginValidator = async (email, password) => {
-//     const user = {email: email, password: password}
-//     const response = await axios.post(`${BASE_URL}/user/signin`, user)
-//     login(response.data);
-//   }
-
-  // const handleMouseDownPassword = (e) => {
-  //   e.preventDefault();
-  // };
+  const loginValidator = async (email, password) => {
+    const user = { email: email, password: password }
+    const response = await axios.post(`${BASE_URL}/user/signin`, user)
+    login(response.data)
+  }
 
   const handleSubmit = async (e) => {
+    e.preventDefault();
+    await doRequest();
+  }
 
-  };
-    return (
-        <Wrapper>
-            <main className='main'>
-        <div className='left'>
-          <div className='left_inner'>
-            <p className='text'>Rate and review your courses on Rate My Courses</p>
-            <img className='register_img' src={register} alt="register" />
+  return (
+    <Wrapper>
+      <main className="main">
+        <div className="left">
+          <div className="left_inner">
+            <p className="text">Rate and review your courses on Rate My Courses</p>
+            <img className="register_img" src={register} alt="register" />
           </div>
         </div>
-        <div className='right'>
+        <div className="right">
           <div className="form_wrapper">
-            <p className='login'>Already has an account? <Link to='/login' className='link link_to_login'>Login</Link></p>
+            <p className="login">
+              Already has an account?{' '}
+              <Link to="/login" className="link link_to_login">
+                Login
+              </Link>
+            </p>
             <p className="form_title">Get started absolutely free.</p>
-            <p className='form_title1'>Free forever. No credit card needed.</p>
+            <p className="form_title1">Free forever. No credit card needed.</p>
             <form onSubmit={handleSubmit}>
-              <input autoComplete="off" type="email" name="email" value={values.email} placeholder='E-mail'
-                    onChange={handleChange} required />
-              <input autoComplete="off" type="text" name="name" value={values.name} placeholder='Username'
-                    onChange={handleChange} required />
-              <input autoComplete="off" type={!showPassword ? "password" : "text"} name="password" value={values.password}
-                    placeholder='Password'
-                    onChange={handleChange} required />
-              {!showPassword ? <VisibilityOffIcon onClick={handleClickShowPassword} className='seen'/> :
-                  <VisibilityIcon onClick={handleClickShowPassword} className='seen'/>}
-              <button className={!loading ? 'submit' : 'submitLoading'}>{ !loading ? 'Sign Up' : <div class="lds-ellipsis"><div></div><div></div><div></div><div></div></div> }</button>
+              <input
+                autoComplete="off"
+                type="email"
+                name="email"
+                value={values.email}
+                placeholder="E-mail"
+                onChange={handleChange}
+                required
+              />
+              <input
+                autoComplete="off"
+                type="text"
+                name="name"
+                value={values.name}
+                placeholder="Username"
+                onChange={handleChange}
+                required
+              />
+              <input
+                autoComplete="off"
+                type={!showPassword ? 'password' : 'text'}
+                name="password"
+                value={values.password}
+                placeholder="Password"
+                onChange={handleChange}
+                required
+              />
+              {!showPassword ? (
+                <VisibilityOffIcon onClick={handleClickShowPassword} className="seen" />
+              ) : (
+                <VisibilityIcon onClick={handleClickShowPassword} className="seen" />
+              )}
+              <button className={!loading ? 'submit' : 'submitLoading'}>
+                {!loading ? (
+                  'Sign Up'
+                ) : (
+                  <div class="lds-ellipsis">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                  </div>
+                )}
+              </button>
             </form>
-            {/* {errors} */}
+            {errors}
           </div>
         </div>
       </main>
-        </Wrapper>
-    );
+    </Wrapper>
+  )
 }
 
 export default SignupComponent
